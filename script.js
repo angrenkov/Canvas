82ce3e72375df0d9e9ffd4378a800c42e958463f
Anton <a_grenkov@yahoo.com>
2018-10-26 10:34:37 +0300

    Added new functional.

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 let canvas = document.getElementById('canvas-area');
 let ctx = canvas.getContext('2d');
-document.getElementById('delete-figure').disabled = true;
-let min = 10;
-let max = 560;
-let minLength = 30;
-let maxLength = 200;
+let deleteButton = document.getElementById('delete-figure');
+deleteButton.disabled = true;
 let selected = -1;
 let mousePressed = {
     move: false,
@@ -16,21 +13,31 @@ let mousePressed = {
     resizeLeft: false,
     resizeBottom: false,
     resizeRight: false,
+    rotate: false,
 };
 let xDiffer = 0;
 let yDiffer = 0;
+let angle = 0;
 const INACCUR = 25;
+const X_OFFSET = 422;
+const Y_OFFSET = 297;
+const MIN = 10;
+const MAX = 560;
+const MIN_LENGTH = 30;
+const MAX_LENGTH = 200;
+const IMAGE_SIDE = 20;
+const CIRCLE_OFFSET = 50;
 
-canvas.style.cursor = 'pointer';
+canvas.style.cursor = 'default';
 
 function getRandomPosition() {
-    return Math.random() * (max - min) + min;
+    return Math.random() * (MAX - MIN) + MIN;
 }
 
 function getRandomLength(coord) {
     let length;
     do {
-        length = Math.random() * (maxLength - minLength) + minLength;
+        length = Math.random() * (MAX_LENGTH - MIN_LENGTH) + MIN_LENGTH;
     } while (coord + length > 590);
     return length;
 }
@@ -47,11 +54,20 @@ function strokeRect(x, y, w, h) {
     ctx.strokeRect(x, y, w, h);
 }
 
+function addImage(x, y, w) {
+    let xImage = x + w / 2 - IMAGE_SIDE / 2;
+    let yImage = y - CIRCLE_OFFSET;
+    let imageCircle = new Image();
+    imageCircle.src = 'images/circle.png';
+    ctx.drawImage(imageCircle, xImage, yImage, IMAGE_SIDE, IMAGE_SIDE);
+}
+
 function Rect(x, y, w, h) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
+    this.a = 0;
 }
 
 Rect.prototype = {
@@ -61,50 +77,72 @@ Rect.prototype = {
 
     stroke: function () {
         strokeRect(this.x, this.y, this.w, this.h);
+    },
+
+    addImg: function () {
+        addImage(this.x, this.y, this.w, this.h);
     }
 };
 
 let i = -1;
 let rect = [];
+update();
 
 function isCursorOnRect(x, y, rect) {
-    return x + INACCUR > rect.x && x - INACCUR < rect.x + rect.w &&
-        y + INACCUR > rect.y && y - INACCUR < rect.y + rect.h;
+    console.log(rect.x + (Math.cos((90 + rect.a) * Math.PI / 180) * rect.h));
+    return x > rect.x + (Math.cos((90 + rect.a) * Math.PI / 180) * rect.h)
+        && x < rect.x + rect.w
+        && y > rect.y
+        && y < rect.y + rect.h;
 }
 
 function isCursorOnTopLeftCorner(x, y, rect) {
-    return x > rect.x && x < rect.x + INACCUR &&
-        y > rect.y && y < rect.y + INACCUR;
+    return x > rect.x
+        && x < rect.x + INACCUR
+        && y > rect.y
+        && y < rect.y + INACCUR;
 }
 
 function isCursorOnTopRight(x, y, rect) {
-    return x > rect.x + rect.w - INACCUR && x < rect.x + rect.w &&
-        y > rect.y && y < rect.y + INACCUR;
+    return x > rect.x + rect.w - INACCUR
+        && x < rect.x + rect.w
+        && y > rect.y
+        && y < rect.y + INACCUR;
 }
 
 function isCursorOnBottomLeft(x, y, rect) {
-    return x > rect.x && x < rect.x + INACCUR &&
-        y > rect.y + rect.h - INACCUR && y < rect.y + rect.h;
+    return x > rect.x
+        && x < rect.x + INACCUR
+        && y > rect.y + rect.h - INACCUR
+        && y < rect.y + rect.h;
 }
 
 function isCursorOnBottomRight(x, y, rect) {
-    return x > rect.x + rect.w - INACCUR && x < rect.x + rect.w &&
-        y > rect.y + rect.h - INACCUR && y < rect.y + rect.h;
+    return x > rect.x + rect.w - INACCUR
+        && x < rect.x + rect.w
+        && y > rect.y + rect.h - INACCUR
+        && y < rect.y + rect.h;
 }
 
 function isCursorOnTop(x, y, rect) {
-    return x > rect.x && x < rect.x + rect.w &&
-        y > rect.y && y < rect.y + INACCUR;
+    return x > rect.x
+        && x < rect.x + rect.w
+        && y > rect.y
+        && y < rect.y + INACCUR;
 }
 
 function isCursorOnLeft(x, y, rect) {
-    return x > rect.x && x < rect.x + INACCUR &&
-        y > rect.y && y < rect.y + rect.h;
+    return x > rect.x
+        && x < rect.x + INACCUR
+        && y > rect.y
+        && y < rect.y + rect.h;
 }
 
 function isCursorOnBottom(x, y, rect) {
-    return x > rect.x && x < rect.x + rect.w &&
-        y > rect.y + rect.h - INACCUR && y < rect.y + rect.h;
+    return x > rect.x
+        && x < rect.x + rect.w
+        && y > rect.y + rect.h - INACCUR
+        && y < rect.y + rect.h;
 }
 
 function isCursorOnRight(x, y, rect) {
@@ -112,61 +150,84 @@ function isCursorOnRight(x, y, rect) {
     const RIGHT_BORDER = rect.x + rect.w;
     const TOP_BORDER = rect.y;
     const BOTTOM_BORDER = rect.y + rect.h;
-    return x > LEFT_BORDER && x < RIGHT_BORDER &&
-        y > TOP_BORDER && y < BOTTOM_BORDER;
+    return x > LEFT_BORDER
+        && x < RIGHT_BORDER
+        && y > TOP_BORDER
+        && y < BOTTOM_BORDER;
 }
 
-canvas.onclick = function (e) {
-    let x = e.pageX - (window.innerWidth / 2 - 425);
-    let y = e.pageY - (window.innerHeight / 2 - 300);
+function getRotateData(rect) {
+    return [rect.x + rect.w / 2 - IMAGE_SIDE / 2,
+        rect.x + rect.w / 2 + IMAGE_SIDE / 2,
+        rect.y - CIRCLE_OFFSET - IMAGE_SIDE / 2,
+        rect.y - CIRCLE_OFFSET + IMAGE_SIDE / 2,
+        rect.x + rect.w / 2,
+        rect.y - CIRCLE_OFFSET];
+}
+
+function isCursorOnRotate(x, y, rect) {
+    let rotateData = getRotateData(rect);
+    return x > rotateData[0]
+        && x < rotateData[1]
+        && y > rotateData[2]
+        && y < rotateData[3];
+}
+
+document.body.onclick = function (e) {
+    let x = e.pageX - (window.innerWidth / 2 - X_OFFSET);
+    let y = e.pageY - (window.innerHeight / 2 - Y_OFFSET);
     for (let index = i; index >= 0; index--) {
-        if (isCursorOnRect(x, y, rect[index])) {
+        if (isCursorOnRect(x, y, rect[index]) || isCursorOnRotate(x, y, rect[index])) {
             selected = index;
             update();
-            document.getElementById('delete-figure').disabled = false;
+            deleteButton.disabled = false;
             return;
         }
     }
     selected = -1;
+    angle = 0;
     update();
 };
 
-canvas.onmousedown = function (e) {
-    if (e.button !== 0 || selected < 0) {
+document.body.onmousedown = function (e) {
+    if (e.button !== 0 || selected < 0) return;
+
+    let x = e.pageX - (window.innerWidth / 2 - X_OFFSET);
+    let y = e.pageY - (window.innerHeight / 2 - Y_OFFSET);
+    if (isCursorOnRotate(x, y, rect[selected])) {
+        mousePressed.rotate = true;
         return;
     }
-    let x = e.pageX - (window.innerWidth / 2 - 422);
-    let y = e.pageY - (window.innerHeight / 2 - 297);
     if (isCursorOnRect(x, y, rect[selected])) {
-        if (isCursorOnTopLeftCorner(x, y, rect[selected])){
+        if (isCursorOnTopLeftCorner(x, y, rect[selected])) {
             mousePressed.resizeTopLeft = true;
             return;
         }
-        if (isCursorOnTopRight(x, y, rect[selected])){
+        if (isCursorOnTopRight(x, y, rect[selected])) {
             mousePressed.resizeTopRight = true;
             return;
         }
-        if (isCursorOnBottomLeft(x, y, rect[selected])){
+        if (isCursorOnBottomLeft(x, y, rect[selected])) {
             mousePressed.resizeBottomLeft = true;
             return;
         }
-        if (isCursorOnBottomRight(x, y, rect[selected])){
+        if (isCursorOnBottomRight(x, y, rect[selected])) {
             mousePressed.resizeBottomRight = true;
             return;
         }
-        if (isCursorOnTop(x, y, rect[selected])){
+        if (isCursorOnTop(x, y, rect[selected])) {
             mousePressed.resizeTop = true;
             return;
         }
-        if (isCursorOnLeft(x, y, rect[selected])){
+        if (isCursorOnLeft(x, y, rect[selected])) {
             mousePressed.resizeLeft = true;
             return;
         }
-        if (isCursorOnBottom(x, y, rect[selected])){
+        if (isCursorOnBottom(x, y, rect[selected])) {
             mousePressed.resizeBottom = true;
             return;
         }
-        if (isCursorOnRight(x, y, rect[selected])){
+        if (isCursorOnRight(x, y, rect[selected])) {
             mousePressed.resizeRight = true;
             return;
         }
@@ -176,13 +237,49 @@ canvas.onmousedown = function (e) {
     }
 };
 
-canvas.onmouseup = function () {
+document.body.onmouseup = function () {
     for (let key in mousePressed) {
         mousePressed[key] = false;
     }
 };
 
-canvas.onmousemove = function (e) {
+document.body.onmousemove = function (e) {
+    const x = e.pageX - (window.innerWidth / 2 - X_OFFSET);
+    const y = e.pageY - (window.innerHeight / 2 - Y_OFFSET);
+    let isOverRect = false;
+    for (let elem of rect) {
+        if (isCursorOnRect(x, y, elem)) {
+            canvas.style.cursor = 'pointer';
+            isOverRect = true;
+        }
+        if (isCursorOnRight(x, y, elem)) {
+            canvas.style.cursor = 'ew-resize';
+        }
+        if (isCursorOnBottom(x, y, elem)) {
+            canvas.style.cursor = 'n-resize';
+        }
+        if (isCursorOnLeft(x, y, elem)) {
+            canvas.style.cursor = 'ew-resize';
+        }
+        if (isCursorOnTop(x, y, elem)) {
+            canvas.style.cursor = 'n-resize';
+        }
+        if (isCursorOnBottomRight(x, y, elem)) {
+            canvas.style.cursor = 'nw-resize';
+        }
+        if (isCursorOnBottomLeft(x, y, elem)) {
+            canvas.style.cursor = 'ne-resize';
+        }
+        if (isCursorOnTopRight(x, y, elem)) {
+            canvas.style.cursor = 'ne-resize';
+        }
+        if (isCursorOnTopLeftCorner(x, y, elem)) {
+            canvas.style.cursor = 'nw-resize';
+        }
+    }
+    if (!isOverRect) {
+        canvas.style.cursor = 'default';
+    }
     let mouseEvent = -1;
     let arrayOfMousePressed = Object.values(mousePressed);
     for (let [index, key] of arrayOfMousePressed.entries()) {
@@ -190,11 +287,8 @@ canvas.onmousemove = function (e) {
             mouseEvent = index;
         }
     }
-    if (mouseEvent === -1) {
-        return;
-    }
-    let x = e.pageX - (window.innerWidth / 2 - 422);
-    let y = e.pageY - (window.innerHeight / 2 - 297);
+    if (mouseEvent === -1) return;
+
     switch (mouseEvent) {
         case 0: {
             rect[selected].x = x - xDiffer;
@@ -252,6 +346,12 @@ canvas.onmousemove = function (e) {
             update();
             break;
         }
+        case 9: {
+            const ROTATE_DATA = getRotateData(rect[selected]);
+            rect[selected].a = x - ROTATE_DATA[4];
+            update();
+            break;
+        }
         default: {
             break;
         }
@@ -280,23 +380,31 @@ function deleteElem(index) {
 function deleteRect() {
     if (selected > -1) {
         deleteElem(selected);
-        document.getElementById('delete-figure').disabled = true;
+        deleteButton.disabled = true;
     }
 }
 
 function clearArea() {
     rect = [];
-    update();
-    i = -1;
     selected = -1;
+    i = -1;
+    update();
 }
 
 function update() {
     ctx.clearRect(0, 0, 600, 600);
-    for (let i in rect) {
+    for (let i = 0; i < rect.length; i++) {
+        ctx.save();
+        const X = rect[i].x + rect[i].w / 2;
+        const Y = rect[i].y + rect[i].h / 2;
+        ctx.translate(X, Y);
+        ctx.rotate(rect[i].a * (Math.PI / 180));
+        ctx.translate(-X, -Y);
         rect[i].draw();
-    }
-    if (selected > -1) {
-        rect[selected].stroke();
+        if (selected === i) {
+            rect[selected].stroke();
+            rect[selected].addImg();
+        }
+        ctx.restore();
     }
 }
