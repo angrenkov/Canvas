8426a75c8489ed77684f41456b87600cd1243156
Anton <a_grenkov@yahoo.com>
2018-11-07 16:12:25 +0300

    Rotate fixed.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let canvas = document.getElementById('canvas-area');
 let ctx = canvas.getContext('2d');
 let deleteButton = document.getElementById('delete-figure');
+
 deleteButton.disabled = true;
+
 let selected = -1;
 let mousePressed = {
     move: false,
@@ -17,7 +19,9 @@ let mousePressed = {
 };
 let xDiffer = 0;
 let yDiffer = 0;
-let angle = 0;
+let initialX = 0;
+let initialY = 0;
+
 const INACCUR = 25;
 const X_OFFSET = 422;
 const Y_OFFSET = 297;
@@ -27,6 +31,8 @@ const MIN_LENGTH = 30;
 const MAX_LENGTH = 200;
 const IMAGE_SIDE = 20;
 const CIRCLE_OFFSET = 50;
+const ANGLIFY = Math.PI / 180;
+
 
 canvas.style.cursor = 'default';
 
@@ -71,15 +77,15 @@ function Rect(x, y, w, h) {
 }
 
 Rect.prototype = {
-    draw: function () {
+    draw() {
         fillRect(this.x, this.y, this.w, this.h);
     },
 
-    stroke: function () {
+    stroke() {
         strokeRect(this.x, this.y, this.w, this.h);
     },
 
-    addImg: function () {
+    addImg() {
         addImage(this.x, this.y, this.w, this.h);
     }
 };
@@ -88,12 +94,36 @@ let i = -1;
 let rect = [];
 update();
 
+function positivateAngle(angle) {
+    return angle % 360 + 360;
+}
+
+function anglifier(angle) {
+    return angleCounter(angle) * ANGLIFY;
+}
+
+function angleCounter(angle) {
+    return (positivateAngle(angle) % 90);
+}
+
+function sectionCounter(angle) {
+    return !!(positivateAngle(angle) / 90 % 2);
+}
+
 function isCursorOnRect(x, y, rect) {
-    console.log(rect.x + (Math.cos((90 + rect.a) * Math.PI / 180) * rect.h));
-    return x > rect.x + (Math.cos((90 + rect.a) * Math.PI / 180) * rect.h)
-        && x < rect.x + rect.w
-        && y > rect.y
-        && y < rect.y + rect.h;
+    const side1 = Math.cos(anglifier(rect.a)) * rect.w
+        + Math.sin(anglifier(rect.a)) * rect.h;
+    const side2 = Math.sin(anglifier(rect.a)) * rect.w
+        + Math.cos(anglifier(rect.a)) * rect.h;
+    const w = (!sectionCounter(rect.a)) ? side1 : side2;
+    const h = (!sectionCounter(rect.a)) ? side2 : side1;
+    const xTransformed = rect.x + (rect.w - w) / 2;
+    const yTransformed = rect.y + (rect.h - h) / 2;
+
+    return x > xTransformed - INACCUR
+        && x < xTransformed + w + INACCUR
+        && y >= yTransformed - INACCUR
+        && y <= yTransformed + h + INACCUR
 }
 
 function isCursorOnTopLeftCorner(x, y, rect) {
@@ -150,6 +180,7 @@ function isCursorOnRight(x, y, rect) {
     const RIGHT_BORDER = rect.x + rect.w;
     const TOP_BORDER = rect.y;
     const BOTTOM_BORDER = rect.y + rect.h;
+
     return x > LEFT_BORDER
         && x < RIGHT_BORDER
         && y > TOP_BORDER
@@ -167,6 +198,7 @@ function getRotateData(rect) {
 
 function isCursorOnRotate(x, y, rect) {
     let rotateData = getRotateData(rect);
+
     return x > rotateData[0]
         && x < rotateData[1]
         && y > rotateData[2]
@@ -176,6 +208,7 @@ function isCursorOnRotate(x, y, rect) {
 document.body.onclick = function (e) {
     let x = e.pageX - (window.innerWidth / 2 - X_OFFSET);
     let y = e.pageY - (window.innerHeight / 2 - Y_OFFSET);
+
     for (let index = i; index >= 0; index--) {
         if (isCursorOnRect(x, y, rect[index]) || isCursorOnRotate(x, y, rect[index])) {
             selected = index;
@@ -183,17 +216,39 @@ document.body.onclick = function (e) {
             deleteButton.disabled = false;
             return;
         }
+        if (!isCursorOnRect(x, y, rect[index])) {
+            deleteButton.disabled = true;
+            ctx.restore();
+        }
     }
     selected = -1;
-    angle = 0;
+    ctx.restore();
     update();
 };
 
 document.body.onmousedown = function (e) {
     if (e.button !== 0 || selected < 0) return;
 
+    ctx.save();
+
     let x = e.pageX - (window.innerWidth / 2 - X_OFFSET);
     let y = e.pageY - (window.innerHeight / 2 - Y_OFFSET);
+
+    initialX = x;
+    initialY = y;
+
+    const X = rect[selected].x + rect[selected].w / 2;
+    const Y = rect[selected].y + rect[selected].h / 2;
+    const ANGLE = rect[selected].a * (Math.PI / 180);
+
+    ctx.translate(X, Y);
+
+    if (ANGLE != 0) {
+        ctx.rotate(ANGLE);
+    }
+
+    ctx.translate(-X, -Y);
+
     if (isCursorOnRotate(x, y, rect[selected])) {
         mousePressed.rotate = true;
         return;
@@ -246,42 +301,46 @@ document.body.onmouseup = function () {
 document.body.onmousemove = function (e) {
     const x = e.pageX - (window.innerWidth / 2 - X_OFFSET);
     const y = e.pageY - (window.innerHeight / 2 - Y_OFFSET);
+
     let isOverRect = false;
+
     for (let elem of rect) {
         if (isCursorOnRect(x, y, elem)) {
             canvas.style.cursor = 'pointer';
             isOverRect = true;
         }
         if (isCursorOnRight(x, y, elem)) {
-            canvas.style.cursor = 'ew-resize';
+            canvas.style.cursor = 'grab';
         }
         if (isCursorOnBottom(x, y, elem)) {
-            canvas.style.cursor = 'n-resize';
+            canvas.style.cursor = 'grab';
         }
         if (isCursorOnLeft(x, y, elem)) {
-            canvas.style.cursor = 'ew-resize';
+            canvas.style.cursor = 'grab';
         }
         if (isCursorOnTop(x, y, elem)) {
-            canvas.style.cursor = 'n-resize';
+            canvas.style.cursor = 'grab';
         }
         if (isCursorOnBottomRight(x, y, elem)) {
-            canvas.style.cursor = 'nw-resize';
+            canvas.style.cursor = 'grab';
         }
         if (isCursorOnBottomLeft(x, y, elem)) {
-            canvas.style.cursor = 'ne-resize';
+            canvas.style.cursor = 'grab';
         }
         if (isCursorOnTopRight(x, y, elem)) {
-            canvas.style.cursor = 'ne-resize';
+            canvas.style.cursor = 'grab';
         }
         if (isCursorOnTopLeftCorner(x, y, elem)) {
-            canvas.style.cursor = 'nw-resize';
+            canvas.style.cursor = 'grab';
         }
     }
     if (!isOverRect) {
         canvas.style.cursor = 'default';
     }
+
     let mouseEvent = -1;
     let arrayOfMousePressed = Object.values(mousePressed);
+
     for (let [index, key] of arrayOfMousePressed.entries()) {
         if (key) {
             mouseEvent = index;
@@ -293,6 +352,7 @@ document.body.onmousemove = function (e) {
         case 0: {
             rect[selected].x = x - xDiffer;
             rect[selected].y = y - yDiffer;
+            ctx.restore();
             update();
             break;
         }
@@ -301,6 +361,7 @@ document.body.onmousemove = function (e) {
             rect[selected].h += rect[selected].y - y;
             rect[selected].x = x;
             rect[selected].y = y;
+            ctx.restore();
             update();
             break;
         }
@@ -308,6 +369,7 @@ document.body.onmousemove = function (e) {
             rect[selected].w = x - rect[selected].x;
             rect[selected].h += rect[selected].y - y;
             rect[selected].y = y;
+            ctx.restore();
             update();
             break;
         }
@@ -315,46 +377,56 @@ document.body.onmousemove = function (e) {
             rect[selected].w += rect[selected].x - x;
             rect[selected].h = y - rect[selected].y;
             rect[selected].x = x;
+            ctx.restore();
             update();
             break;
         }
         case 4: {
             rect[selected].w = x - rect[selected].x;
             rect[selected].h = y - rect[selected].y;
+            ctx.restore();
             update();
             break;
         }
         case 5: {
-            rect[selected].h += rect[selected].y - y;
-            rect[selected].y = y;
+            rect[selected].h += initialY - y;
+            rect[selected].y += y - initialY;
+            initialY = y;
+            ctx.restore();
             update();
             break;
         }
         case 6: {
-            rect[selected].w += rect[selected].x - x;
-            rect[selected].x = x;
+            rect[selected].w += initialX - x;
+            rect[selected].x += x - initialX;
+            initialX = x;
+            ctx.restore();
             update();
             break;
         }
         case 7: {
-            rect[selected].h = y - rect[selected].y;
+            rect[selected].h += y - initialY;
+            initialY = y;
+            ctx.restore();
             update();
             break;
         }
         case 8: {
-            rect[selected].w = x - rect[selected].x;
+            rect[selected].w += x - initialX;
+            initialX = x;
+            ctx.restore();
             update();
             break;
         }
         case 9: {
-            const ROTATE_DATA = getRotateData(rect[selected]);
-            rect[selected].a = x - ROTATE_DATA[4];
+            rect[selected].a += x - initialX;
+            initialX = x;
+            ctx.restore();
             update();
             break;
         }
-        default: {
+        default:
             break;
-        }
     }
 };
 
@@ -367,12 +439,14 @@ function drawRect() {
     } else {
         alert('The canvas is full!');
     }
+    ctx.restore();
     update();
 }
 
 function deleteElem(index) {
     rect.splice(index, 1);
     selected = -1;
+    ctx.restore();
     update();
     i--;
 }
@@ -388,23 +462,35 @@ function clearArea() {
     rect = [];
     selected = -1;
     i = -1;
+    ctx.restore();
     update();
 }
 
 function update() {
     ctx.clearRect(0, 0, 600, 600);
+    ctx.restore();
     for (let i = 0; i < rect.length; i++) {
         ctx.save();
+
         const X = rect[i].x + rect[i].w / 2;
         const Y = rect[i].y + rect[i].h / 2;
+        const ANGLE = rect[i].a * (Math.PI / 180);
+
         ctx.translate(X, Y);
-        ctx.rotate(rect[i].a * (Math.PI / 180));
+
+        if (ANGLE != 0) {
+            ctx.rotate(ANGLE);
+        }
+
         ctx.translate(-X, -Y);
         rect[i].draw();
+
         if (selected === i) {
             rect[selected].stroke();
-            rect[selected].addImg();
         }
         ctx.restore();
+    }
+    if (selected > -1) {
+        rect[selected].addImg();
     }
 }
